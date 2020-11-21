25657b94e09865a8dffd2da498eaac0341759269
tdallas <tdallas@itba.edu.ar>
2020-11-21 11:40:45 -0300

    added two dimmensions

--- mlp_main.py
+++ mlp_main.py
@@ -2,13 +2,14 @@ from mlp import Mlp
 from fonts import get_inputs, get_outputs
 import numpy as np
 
-network = Mlp(size_layers = [35, 100, 35], 
-                         act_funct   = 'relu',
-                         reg_lambda  = 0,
-                         bias_flag   = False)
+network = Mlp(size_layers=[35, 25, 12, 2, 12, 25, 35],
+              act_funct='relu',
+              reg_lambda=0,
+              bias_flag=False)
 
 network.train(get_inputs(), get_outputs())
 
-prediction = network.predict(np.array([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
+prediction = network.predict(np.array(
+    [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]))
 
 print(prediction)
