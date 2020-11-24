64529666ca0c2c79e5f54f4748d8fe48a55c65e6
tdorado <tdorado@itba.edu.ar>
2020-11-24 10:33:19 -0300

    not working

---
+++ ej1.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from mlp import MLP
+from fonts import get_input, get_output
+
+train_x = get_input(2)
+train_y = get_output(2)
+
+ae = MLP([35, 17, 15, 3], 2, [3, 15, 17, 35], activation='tanh', solver='bfgs', eta=0.0001, max_iterations=1500, adapt_eta=False, verbose=True)
+ae.train(train_x, train_x)
+
+for i in range(10):
+    i = np.random.randint(train_x.shape[0])
+    prediction = ae.predict(train_x[i].reshape(-1,35))
+
+    plt.figure()
+    plt.subplot(1,2,1)
+    plt.imshow(train_x[i].reshape(7,5), 'gray_r')
+    plt.title("Input Letter: " + train_y[i], fontsize = 15)
+    plt.xticks([])
+    plt.yticks([])
+    plt.subplot(1,2,2)
+    plt.imshow(prediction.reshape(7,5), 'gray_r')
+    plt.title('Predicted', fontsize = 15)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+
+# latent_space_1 = np.array([-0.8, 0.8])
+# prediction_1 = ae.decode(latent_space_1)
+# latent_space_2 = np.array([0.7, -0.7])
+# prediction_2 = ae.decode(latent_space_2)
+
+# plt.figure()
+# plt.subplot(2,2,1)
+# plt.imshow(latent_space_1.reshape(2,1), 'gray_r')
+# plt.title('Latent Space', fontsize = 15)
+# plt.xticks([])
+# plt.yticks([])
+# plt.subplot(2,2,2)
+# plt.imshow(prediction_1.reshape(7,5), 'gray_r')
+# plt.title('Predicted', fontsize = 15)
+# plt.xticks([])
+# plt.yticks([])
+# plt.subplot(2,2,3)
+# plt.imshow(latent_space_2.reshape(2,1), 'gray_r')
+# plt.xticks([])
+# plt.yticks([])
+# plt.subplot(2,2,4)
+# plt.imshow(prediction_2.reshape(7,5), 'gray_r')
+# plt.xticks([])
+# plt.yticks([])
+# plt.show()
--- mlp.py
+++ mlp.py
@@ -1,63 +1,86 @@
 import numpy as np
 import math
 from scipy.optimize import minimize
-from fonts import get_inputs, get_inputs_with_bias
+
+def relu(z):
+    if np.isscalar(z):
+        result = np.max((z, 0))
+    else:
+        zero_aux = np.zeros(z.shape)
+        meta_z = np.stack((z, zero_aux), axis=-1)
+        result = np.max(meta_z, axis=-1)
+    return result
+
+def relu_derivative( z):
+    return 1 * (z > 0)
+
+def tanh(z):
+    return np.tanh(0.3 * z)
+
+def tanh_derivative(z):
+    return 0.3 * (1 - np.tanh(z) ** 2)
+
+def logistic(z):
+    return 1 / (1 + np.exp(-2 * 0.3 * z))
+
+def logistic_derivative(z):
+    act = logistic(z)
+    return 2 * 0.3 * act * (1 - act)
 
 
-class Mlp():
-    def __init__(self, size_layers, act_funct='relu', reg_lambda=0.9, with_bias=True):
-        '''
-        Constructor method. Defines the characteristics of the MLP
-        Arguments:
-            size_layers : List with the number of Units for:
-                [Input, Hidden1, Hidden2, ... HiddenN, Output] Layers.
-            act_funtc   : Activation function for all the Units in the MLP
-                default = 'relu'
-            reg_lambda: Value of the regularization parameter Lambda
-                lambda = 0, i.e. no regularization
-            bias: Indicates is the bias element is added for each layer, but the output
-        '''
-        self.size_layers = size_layers
-        self.n_layers = len(size_layers)
-        self.act_f = act_funct
-        self.eta = reg_lambda
+activation_functions = {
+    "tanh": tanh,
+    "logistic": logistic,
+    "relu": relu
+}
+
+derivative_activation_functions = {
+    "tanh": tanh_derivative,
+    "logistic": logistic_derivative,
+    "relu": relu_derivative
+}
+
+class MLP():
+    def __init__(self, encoder_layer_sizes, latent_layer_size, decoder_layer_sizes, activation, solver, eta=0.0001, max_iterations=30000, adapt_eta=False, with_bias=True, verbose=False):
+        self.encoder_layer = len(encoder_layer_sizes)
+        self.latent_layer = self.encoder_layer + 1
+        self.decoder_layer = self.latent_layer + len(decoder_layer_sizes) + 1
+        aux = encoder_layer_sizes
+        aux.append(latent_layer_size)
+        self.layer_sizes = aux + decoder_layer_sizes
+        self.n_layers = len(self.layer_sizes)
+        self.activation = activation_functions[activation]
+        self.derivate_activation = derivative_activation_functions[activation]
+        self.solver = solver
+        self.max_iterations = max_iterations
+        self.eta = eta
         self.with_bias = with_bias
+        self.adapt_eta = adapt_eta
+        self.verbose = verbose
         self.prev_error = None
+        self.weights = self.initialize_weights()
 
-        # Ramdomly initialize theta (MLP weights)
-        self.initialize_theta_weights()
-
-    def train(self, X, Y, iterations=50, reset=True):
-        '''
-        Given X (feature matrix) and y (class vector)
-        Updates the Theta Weights by running Backpropagation N tines
-        Arguments:
-            X          : Feature matrix [n_examples, n_features]
-            Y          : Sparse class matrix [n_examples, classes]
-            iterations : Number of times Backpropagation is performed
-                default = 400
-            reset      : If set, initialize Weights before training
-                default = False
-        '''
+    def train(self, training_input, training_output, reset=True):
         if reset:
-            self.initialize_theta_weights()
-        for i in range(iterations):
-            # self.gradients = self.backpropagation(X, Y)
-            # self.gradients_vector = self.unroll_weights(self.gradients)
-            # self.theta_vector = self.unroll_weights(self.weights)
-            # self.theta_vector = self.theta_vector - self.gradients_vector
-            # self.weights = self.roll_weights(self.theta_vector)
-            self.backpropagation(X, Y)
-            self.eta = self.adapt_eta()
+            self.initialize_weights()
+        for i in range(self.max_iterations):
+            if self.verbose:
+                print("Iteration: " + str(i))
+            self.gradients = self.backpropagation(training_input, training_output)
+            self.gradients_vector = self.unroll_weights(self.gradients)
+            self.theta_vector = self.unroll_weights(self.weights)
+            self.theta_vector = self.theta_vector - self.gradients_vector
+            self.weights = self.roll_weights(self.theta_vector)
+            if self.adapt_eta:
+                self.eta = self.adapt_eta(training_input, training_output)
+        self.mimize_weights_error(training_input, training_output)
 
-    def adapt_eta(self):
-        inputs_to_calculate_error = get_inputs()[:8]
+    def adapt_eta(self, input_t, output_t):
         error = 0
-        for inputs in inputs_to_calculate_error:
-            error_calculated = self.calculate_error([inputs], [inputs])
+        for inputs in input_t:
+            error_calculated = self.calculate_error(input_t, output_t)
             error += error_calculated
-        error = error / len(inputs_to_calculate_error)
-        # print('error', error)
+        error = error / len(input_t)
         if self.prev_error is None:
             self.prev_error = error
             return self.eta + (error * 0.000001)
@@ -72,21 +95,12 @@ class Mlp():
                 self.prev_error = error
                 return self.eta + (error * 0.000001)
 
-    def predict(self, X):
-        '''
-        Given X (feature matrix), y_hay is computed
-        Arguments:
-            X      : Feature matrix [n_examples, n_features]
-        Output:
-            y_hat  : Computed Vector Class for X
-        '''
-        # print('llamando a predict con', X)
-        A, Z = self.feedforward(X)
-        # print('A', A[-1])
+    def predict(self, input_t):
+        A, Z = self.feedforward(input_t)
         Y_hat = A[-1]
         return Y_hat
 
-    def initialize_theta_weights(self):
+    def initialize_weights(self):
         '''
         Initialize weights, initialization method depends
         on the Activation Function and the Number of Units in the current layer
@@ -94,9 +108,9 @@ class Mlp():
         The weights for each layer as of the size [next_layer, current_layer + 1]
         '''
         self.weights = []
-        size_next_layers = self.size_layers.copy()
+        size_next_layers = self.layer_sizes.copy()
         size_next_layers.pop(0)
-        for size_layer, size_next_layer in zip(self.size_layers, size_next_layers):
+        for size_layer, size_next_layer in zip(self.layer_sizes, size_next_layers):
             epsilon = np.sqrt(2.0 / (size_layer * size_next_layer))
             # Weigts from Normal distribution mean = 0, std = epsion
             if self.with_bias:
@@ -108,19 +122,15 @@ class Mlp():
             self.weights.append(theta_tmp)
         return self.weights
 
-    def backpropagation(self, X, Y):
-        '''
-        Implementation of the Backpropagation algorithm with regularization
-        '''
-        def g_dz(x): return self.relu_derivative(x)
+    def backpropagation(self, input_t, output_t):
 
-        n_examples = X.shape[0]
+        n_examples = input_t.shape[0]
         # Feedforward
-        A, Z = self.feedforward(X)
+        A, Z = self.feedforward(input_t)
 
         # Backpropagation
         deltas = [None] * self.n_layers
-        deltas[-1] = A[-1] - Y
+        deltas[-1] = A[-1] - output_t
         # For the second last layer to the second one
         for ix_layer in np.arange(self.n_layers - 1 - 1, 0, -1):
             theta_tmp = self.weights[ix_layer]
@@ -128,7 +138,7 @@ class Mlp():
                 # Removing weights for bias
                 theta_tmp = np.delete(theta_tmp, np.s_[0], 1)
             deltas[ix_layer] = (np.matmul(theta_tmp.transpose(
-            ), deltas[ix_layer + 1].transpose())).transpose() * g_dz(Z[ix_layer]) * self.eta
+            ), deltas[ix_layer + 1].transpose())).transpose() * self.derivate_activation(Z[ix_layer]) * self.eta
 
         # Compute gradients
         gradients = [None] * (self.n_layers - 1)
@@ -148,15 +158,10 @@ class Mlp():
             gradients[ix_layer] = grads_tmp
         return gradients
 
-    def feedforward(self, X):
-        '''
-        Implementation of the Feedforward
-        '''
-        def g(x): return self.relu(x)
-
+    def feedforward(self, input_t):
         A = [None] * self.n_layers
         Z = [None] * self.n_layers
-        input_layer = X
+        input_layer = input_t
 
         for ix_layer in range(self.n_layers - 1):
             n_examples = input_layer.shape[0]
@@ -171,30 +176,27 @@ class Mlp():
             Z[ix_layer + 1] = np.matmul(input_layer,
                                         self.weights[ix_layer].transpose())
             # Activation Function
-            output_layer = g(Z[ix_layer + 1])
+            output_layer = self.activation(Z[ix_layer + 1])
             # Current output_layer will be next input_layer
             input_layer = output_layer
 
         A[self.n_layers - 1] = output_layer
         return A, Z
 
-    def calculate_error(self, test_data, test_exp):
-        guesses = [self.predict(np.array([i])) for i in test_data]
+    def calculate_error(self, input_t, output_t):
+        guesses = [self.predict(np.array([i])) for i in input_t]
         return np.sum(
-            [(np.subtract(test_exp[i], guesses[i]) ** 2).sum()
-             for i in range(len(test_exp))]
-        ) / len(test_data)
+            [(np.subtract(output_t[i], guesses[i]) ** 2).sum()
+             for i in range(len(output_t))]
+        ) / len(input_t)
 
-    def cost(self, flat_weights, test_data, test_exp):
-        return self.calculate_error(test_data, test_exp)
+    def cost(self, flat_weights, input_t, output_t):
+        return self.calculate_error(input_t, output_t)
 
-    def cost_denoise(self):
-        return ''
-
-    def mimize_weights_error(self):
+    def mimize_weights_error(self, input_t, output_t):
         prev_w = self.unroll_weights(self.weights)
         minimized_weights = minimize(fun=self.cost, x0=prev_w, args=(
-            get_inputs(), get_inputs()), method='SLSQP')
+            input_t, output_t), method=self.solver)
         after_w = self.roll_weights(minimized_weights.x)
         self.weights = after_w
 
@@ -218,14 +220,14 @@ class Mlp():
         Unrolls a single vector to a list of matrices
         Each matrix represents the Weights (or Gradients) from one layer to the next
         '''
-        size_next_layers = self.size_layers.copy()
+        size_next_layers = self.layer_sizes.copy()
         size_next_layers.pop(0)
         rolled_list = []
         if self.with_bias:
             extra_item = 1
         else:
             extra_item = 0
-        for size_layer, size_next_layer in zip(self.size_layers, size_next_layers):
+        for size_layer, size_next_layer in zip(self.layer_sizes, size_next_layers):
             n_weights = (size_next_layer * (size_layer + extra_item))
             data_tmp = unrolled_data[0: n_weights]
             data_tmp = data_tmp.reshape(
@@ -233,39 +235,3 @@ class Mlp():
             rolled_list.append(data_tmp)
             unrolled_data = np.delete(unrolled_data, np.s_[0:n_weights])
         return rolled_list
-
-    def relu(self, z):
-        return self.tanh(z)
-        '''
-        Rectified Linear function
-        z can be an numpy array or scalar
-        '''
-        if np.isscalar(z):
-            result = np.max((z, 0))
-        else:
-            zero_aux = np.zeros(z.shape)
-            meta_z = np.stack((z, zero_aux), axis=-1)
-            result = np.max(meta_z, axis=-1)
-        return result
-
-    def relu_derivative(self, z):
-        return self.tanh_deriv(z)
-        '''
-        Derivative for Rectified Linear function
-        z can be an numpy array or scalar
-        '''
-        result = 1 * (z > 0)
-        return result
-
-    def tanh(self, x):
-        return np.tanh(0.3 * x)
-
-    def tanh_deriv(self, x):
-        return 0.3 * (1 - np.tanh(x) ** 2)
-
-    def logistic(self, x):
-        return 1 / (1 + np.exp(-2 * 0.3 * x))
-
-    def logistic_deriv(self, x):
-        act = self.logistic(x)
-        return 2 * 0.3 * act * (1 - act)
